156bb2ccfd01574ab37e755dd9323b5e5dd20cd7
Lee <leeamym@dunwoody.edu>
2025-10-20 12:54:10 -0500

    main

--- app.py
+++ app.py
@@ -28,28 +28,24 @@ def pickapet():
     
     pickedpet = None
     namedpet = None
-    
-    if 'visited' in session:
-        return redirect('/yourPet')
-    else:
-        if request.method == 'POST':
-            pickedpet = request.form.get('pickedpet', '').strip()
-            namedpet = request.form.get('namedpet', '').strip()
+    if request.method == 'POST':
+        pickedpet = request.form.get('pickedpet', '').strip()
+        namedpet = request.form.get('namedpet', '').strip()
             
-            if pickedpet == 'Dog':
-                new_player = player_stats(
-                    namedpet=namedpet,
-                    pickedpet=pickedpet,
+        if pickedpet == 'Dog':
+            new_player = player_stats(
+                namedpet=namedpet,
+                pickedpet=pickedpet,
                 )
-                db.session.add(new_player)
-                db.session.commit()
-            if pickedpet == 'Cat':
-                new_player = player_stats(
-                    namedpet=namedpet,
-                    pickedpet=pickedpet
+            db.session.add(new_player)
+            db.session.commit()
+        if pickedpet == 'Cat':
+            new_player = player_stats(
+                namedpet=namedpet,
+                pickedpet=pickedpet
                 )
-                db.session.add(new_player)
-                db.session.commit()
+            db.session.add(new_player)
+            db.session.commit()
     return render_template("pickapet.html", pickedpet=pickedpet, namedpet=namedpet, player_stats=player_stats)
 
 @app.route('/yourPet')
@@ -60,22 +56,22 @@ def yourPet():
     current_day = now.day
     current_time = now.strftime("%I:%M:%S %p")
     
-    data = player_stats.query.all()
+    pickedpet = request.form.get('pickedpet', '').strip()
     
-    if 'Dog' and 'Cat' != player_stats:
+    data = player_stats.query.filter_by(pickedpet=pickedpet).first()
+    
+    if 'Dog' == 'Cat' != data.type(pickedpet):
         return redirect(url_for('pickapet'))
     
-    if 'visited' not in session:
-        session['visited'] = True
-        newuser = (f"To take care of your pet, you must complete your tasks. Every time you complete a task, you get currency to buy your pet the things it needs to survive.")
-    
-    if 'cat' in data:
+    if 'Cat' == data.type(pickedpet):
         hunger = 100
         sleep = 100
         health = 100
         fun = 100
         
-        user = player_stats.query.get(id)
+        playerID = request.form.get('playerID', '').strip()
+        
+        user = player_stats.query.filter_by(id=playerID).first()
         
         while hunger > 0:
             hunger -= 1
@@ -86,6 +82,7 @@ def yourPet():
                         db.session.delete(user)
                         db.session.commit()
                         return redirect('pickapet.html')
+                    # delete the user if the pet has 0 health via hunger
 
         while sleep > 0:
             sleep -= 2
@@ -96,6 +93,7 @@ def yourPet():
                         db.session.delete(user)
                         db.session.commit()
                         return redirect('pickapet.html')
+                    # delete the user if the pet has 0 health via sleep
 
         while fun > 0:
             fun -= 2
@@ -103,35 +101,14 @@ def yourPet():
                 db.session.delete(user)
                 db.session.commit()
                 return redirect('pickapet.html')
+            # delete the user if the pet has 0 fun
 
-    return render_template("yourPet.html", newuser=newuser, day=current_day, year=current_year, data=data, month=current_month, time=current_time, hunger=hunger)
+    return render_template("yourPet.html", newuser=newuser, day=current_day, year=current_year, data=data, month=current_month, time=current_time)
 
 @app.route('/adminView')
 def adminView():
     players = player_stats.query.all()
     return render_template('adminView.html', players=players, get_localzone=get_localzone)
-
-@app.route('/adminView/delete_first')
-def admin_Player_deleteFirst():
-    try:
-        all_players = player_stats.query.order_by(player_stats.id).all()
-        first_entry = player_stats.query.first()
-        
-        if first_entry:
-            db.session.delete(first_entry)
-            db.session.commit()
-            return redirect(url_for('adminView'))
-        
-        elif len(all_players) < 1:
-            error = "You have no Players to delete."
-            players = player_stats.query.all()
-            return redirect(url_for('adminView'))
-    
-    except Exception as e:
-        db.session.rollback()
-        error = "An error occured whilst deleting. Try again."
-        return redirect(url_for('adminView'))
-    return render_template('adminView.html', players=players, error=error)
 
 @app.route('/adminView/deleteButton', methods=['POST'])
 def admin_Player_DeleteButton():
